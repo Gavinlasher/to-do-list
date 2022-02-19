6e442d2c9bb333ae9967853fab1ca10d7af7fc36
Gavin Lasher <Krgavin21@yahoo.com>
2022-02-18 17:20:10 -0700

    friday update

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -1,16 +1,18 @@
 import { generateId } from "../Utils/generateId.js";
 
 export class Task {
-  constructor({ name, listid, id }) {
+  constructor({ name, listid, id,check }) {
     this.name = name;
     this.listid = listid;
+    // is it complete? cheack that needs to reurn true or false 
+ this.check = 
     this.id = generateId() || id;
   }
-
+// checked turnary for a checked box below
   get taskTemplate() {
     return /*html*/ `
     <div class="form-check bg-secondary text-center">
-    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+    <input class="form-check-input" type="checkbox" value="" ${ ? 'checked': ''} id="defaultCheck1">
     <label class="form-check-label" for="defaultCheck1">
       ${this.name}
     </label>
